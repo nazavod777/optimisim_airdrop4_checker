e9d16ccf661c63dde2c5ce49f0a0b5f719f0cd25
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-02-21 00:36:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from sys import stderr, platform
 from loguru import logger
 
 from core import check_account
-from custom_types import FormattedAccount
-from utils import format_account
 from utils import loader
 
 if platform == 'win32':
@@ -41,11 +39,8 @@ if __name__ == '__main__':
             if not data:
                 break
 
-            accounts_list: list[FormattedAccount] = [current_account for current_account in
-                                                     [format_account(account_data=row)
-                                                      for row in list(set([row.strip().rstrip()
-                                                                           for row in data.split('\n')]))]
-                                                     if current_account]
+            accounts_list: list[str] = [row for row in list(set([row.strip().rstrip()
+                                                                 for row in data.split('\n')]))]
 
             logger.info(f'Loaded Accounts: {len(accounts_list)}')
 
