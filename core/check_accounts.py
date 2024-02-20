d585578cbf436f1f557b1ba0d1af16f983d20933
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-02-21 00:36:44 +0300

    Update check_accounts.py

--- core/check_accounts.py
+++ core/check_accounts.py
@@ -2,20 +2,26 @@ import aiofiles
 from loguru import logger
 
 from custom_types.formatted_account import FormattedAccount
+from utils import format_account
 from .get_eligible_list import eligible_accounts_data
 
 
-async def check_account(account_data: FormattedAccount) -> bool:
-    drop_value: str | None = eligible_accounts_data.get(account_data.address.lower())
+async def check_account(account_data: str) -> bool:
+    formatted_account: FormattedAccount | None = format_account(account_data=account_data)
+
+    if not formatted_account:
+        return False
+
+    drop_value: str | None = eligible_accounts_data.get(formatted_account.address.lower())
 
     if not drop_value:
-        logger.error(f'{account_data.address} | Not Eligible')
+        logger.error(f'{formatted_account.address} | Not Eligible')
         return False
 
     account_data_dict: dict = {
-        'address': account_data.address,
-        'private_key': account_data.private_key,
-        'mnemonic': account_data.mnemonic
+        'address': formatted_account.address,
+        'private_key': formatted_account.private_key,
+        'mnemonic': formatted_account.mnemonic
     }
     account_data_for_file: str = ' | '.join([current_value for current_value
                                              in account_data_dict.values()
@@ -26,4 +32,4 @@ async def check_account(account_data: FormattedAccount) -> bool:
                              encoding='utf-8-sig') as file:
         await file.write(f'{account_data_for_file} | {drop_value}\n')
 
-    logger.success(f'{account_data.address} | Eliginle: {drop_value}')
+    logger.success(f'{formatted_account.address} | Eliginle: {drop_value}')
