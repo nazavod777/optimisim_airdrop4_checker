4c9f4f963f5b015fdb8c74af8eaa5022d2a5464a
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-10-09 22:14:38 +0300

    Update get_eligible_list.py

--- core/get_eligible_list.py
+++ core/get_eligible_list.py
@@ -1,7 +1,6 @@
 import requests
 
-r = requests.get(url='https://raw.githubusercontent.com/ethereum-optimism/op-analytics/main/reference_data/'
-                     'address_lists/op_airdrop_4_simple_list.csv')
+r = requests.get(url='https://raw.githubusercontent.com/ethereum-optimism/op-analytics/refs/heads/main/reference_data/address_lists/op_airdrop_5_simple_list.csv')
 eligible_accounts_data: dict = {}
 
 for current_account in (r.text.split('\n')):
